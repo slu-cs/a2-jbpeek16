01e580dcab63485d9ac8a9a9fd231709ddc25855
jbpeek <jbpeek16@stlawu.edu>
2020-02-14 18:59:12 -0500

    Create voters

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ CS332 Assignment #2
 
 Citations:
 https://docs.mongodb.com/manual/reference/ (mongodb documentation)
+https://www.geeksforgeeks.org/javascript-program-to-read-text-file/ (reading a file)
+https://stackoverflow.com/questions/8125709/javascript-how-to-split-newline (split on newline)
+https://dev.to/jamesliudotcc/how-to-use-async-await-with-map-and-promise-all-1gb5 (Promise.all)
--- create.js
+++ create.js
@@ -2,48 +2,39 @@
 
 const mongoose = require('mongoose');
 const connect = require('./db');
-const Professor = require('./schema');
-const fs = require('fs') 
+const Voter = require('./schema');
+const fs = require('fs')
 
-fs.readFile('./voters.csv', 'utf-8', (err, data) => { 
-    if (err) throw err; 
-  
-    // Converting Raw Buffer to text 
-    // data using tostring function. 
-    console.log(data); 
-}) 
+let file;
+fs.readFile('./voters.csv', 'utf-8', (error, data) => {
+    if (error) throw error;
+    file = data; 
+})
 
+const voterArray = file.split(/\r?\n/).split(",");
+let voterObjectArray = [];
 
-// connect(); // To the database
-//
-// // Create some faculty
-//
-// const harcourt = new Professor({
-//   name: 'Ed Harcourt',
-//   rank: 'Full',
-//   started: 2003,
-//   courses: [140, 220, 345, 362, 364]
-// });
-//
-// const torrey = new Professor({
-//   name: 'Lisa Torrey',
-//   rank: 'Associate',
-//   started: 2009,
-//   courses: [140, 219, 332, 362, 374, 380]
-// });
-//
-// const lee = new Professor({
-//   name: 'Choong-Soo Lee',
-//   rank: 'Associate',
-//   started: 2010,
-//   courses: [140, 219, 256, 321, 370]
-// });
-//
-// // Reset the data
-// mongoose.connection.dropDatabase()
-//   .then(() => harcourt.save())
-//   .then(() => torrey.save())
-//   .then(() => lee.save())
-//   .then(() => mongoose.connection.close())
-//   .then(() => console.log('Database is ready.'))
-//   .catch(error => console.error(error.stack));
+voterArray.array.forEach(voter => {
+  if (voter.length === 4) {
+    voterObjectArray.push(new Voter({
+      name: {first: voter[0], last: voter[1]},
+      zip: voter[2],
+      history: voter[3]
+    }))
+  }
+});
+
+connect(); // To the database
+
+// Reset the data
+mongoose.connection.dropDatabase()
+  .then(voterObjectArray => {
+    Promise.all(
+      voterObjectArray.map(voterObject =>
+        voterObject.save()
+      )
+    )
+  })
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
