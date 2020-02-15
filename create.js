fac45e09accc2a1fc94897c9a6419c2dbca43d83
jbpeek <jbpeek16@stlawu.edu>
2020-02-14 20:17:30 -0500

    CHanges

--- create.js
+++ create.js
@@ -9,29 +9,33 @@ let voterArray = [];
 const file = fs.readFileSync('./voters.csv', 'utf-8')
 
 if (file) voterArray = file.split(/\r?\n/);
-voterArray.map(voter => voter.split(","));
+
+let splitVoterArray = [];
+voterArray.forEach(voter => splitVoterArray.push(voter.split(",")));
 
 let voterObjectArray = [];
-
 voterArray.forEach(voter => {
-  if (voter.length === 4) {
-    voterObjectArray.push(new Voter({
-      name: {first: voter[0], last: voter[1]},
-      zip: voter[2],
-      history: voter[3]
-    }))
-  }
+  const voterHistory = (voter.length === 4) ? voter[3] : ""
+  voterObjectArray.push(new Voter({
+    name: {first: voter[0], last: voter[1]},
+    zip: voter[2],
+    history: voterHistory
+  }))
 });
 
 connect(); // To the database
 
 // Reset the data
 mongoose.connection.dropDatabase()
-  .then(() => {
+  .then(splitVoterArray => {
     Promise.all(
-      voterObjectArray.map(voterObject =>
-        voterObject.save()
-      ).then(response => console.log(response))
+      splitVoterArray.map(voter =>
+        new Voter({
+          name: {first: voter[0], last: voter[1]},
+          zip: voter[2],
+          history: voterHistory
+        }).save()
+      )
     )
   })
   .then(() => mongoose.connection.close())
